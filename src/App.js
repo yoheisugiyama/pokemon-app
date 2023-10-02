bf085321de710304923dd6e2d88fa9bf9fc95fb7
Yohei Sugiyama <yoheisugiyamameister@gmail.com>
2023-10-03 03:41:14 +0900

    15

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import "./App.css"
 import { getAllPokemon, getPokemon } from "./utils/pokemon"
+import Card from "./components/Card/Card"
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon"
@@ -27,15 +28,17 @@ function App() {
     setPokemonData(_pokemonData)
   }
 
-  console.log(pokemonData)
-
   return (
     <div className="App">
       {loading ? (
         <h1>ロード中・・・</h1>
       ) : (
         <>
-          <h1>ポケモンデータを取得しました</h1>
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />
+            })}
+          </div>
         </>
       )}
     </div>
