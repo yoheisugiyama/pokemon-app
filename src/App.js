7fba5a58605457f4e0059b52c7bfa1c35cfd3525
Yohei Sugiyama <yoheisugiyamameister@gmail.com>
2023-10-03 21:04:36 +0900

    20

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import { useEffect, useState } from "react"
 import "./App.css"
 import { getAllPokemon, getPokemon } from "./utils/pokemon"
 import Card from "./components/Card/Card"
+import Navbar from "./components/Navbar/Navbar"
+
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon"
   const [loading, setLoading] = useState(true)
   const [pokemonData, setPokemonData] = useState([])
+  const [nextURL, setNextURL] =useState("")
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       //全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL)
       loadPokemon(res.results)
+      setNextURL(res.next)
       setLoading(false)
     }
     fetchPokemonData()
@@ -25,23 +29,47 @@ function App() {
         return pokemonRecord
       })
     )
+
     setPokemonData(_pokemonData)
   }
 
+  
+  const handlePrevPage =() => {}
+
+
+  const handleNextPage = async() => {
+    setLoading(true)
+    let data = await getAllPokemon(nextURL)
+    setNextURL(data.next)
+    await loadPokemon(data.results)
+    setLoading(false)
+  }
+
+
+  
+
+
   return (
-    <div className="App">
-      {loading ? (
-        <h1>ロード中・・・</h1>
-      ) : (
-        <>
-          <div className="pokemonCardContainer">
-            {pokemonData.map((pokemon, i) => {
-              return <Card key={i} pokemon={pokemon} />
-            })}
-          </div>
-        </>
-      )}
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        {loading ? (
+          <h1>ロード中・・・</h1>
+        ) : (
+          <>
+            <div className="pokemonCardContainer">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />
+              })}
+            </div>
+            <div className="btn">
+              <button onClick={handlePrevPage}>前へ</button>
+              <button onClick={handleNextPage}>次へ</button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   )
 }
 
